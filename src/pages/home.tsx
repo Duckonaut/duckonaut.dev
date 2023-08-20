8302ed4998d2b0e223aefb4b1d1b498de972b4ba
Florian Richer <florian.richer.97@outlook.com>
2023-08-20 02:21:38 +0200

    Fix link in home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -15,8 +15,8 @@ export default function Home() {
         <p>
           Hello! I'm a programmer, game designer, and artist. I'm currently a student at the Warsaw University of
           Technology, studying Computer Science. A list of the games I've worked on is in the <a class="games-link"
-            href="games.html">games</a> section. Other projects I've been a part of are available in the <a class="projects-link"
-              href="projects.html">projects</a> section.
+            href="#/games">games</a> section. Other projects I've been a part of are available in the <a class="projects-link"
+              href="#/projects">projects</a> section.
           I also enjoy some lower-level programming and learning about the inner workings of various systems. The main
           technologies I use are <span class="highlight">C#</span>, <span class="highlight">Unity</span>
           , <span class="highlight">Python</span>, <span class="highlight">Rust</span> and <span class="highlight">C</span>,
