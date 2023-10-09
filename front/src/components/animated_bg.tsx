4d26a164a9340276c2923543a88d6d180326d0de
Stanisław Zagórowski <hasohasacz@gmail.com>
2023-10-09 12:32:49 +0200

    front: add webgl check

--- front/src/components/animated_bg.tsx
+++ front/src/components/animated_bg.tsx
@@ -111,4 +111,16 @@ const timeLoop = (
     };
 }
 
-export const AnimatedBg = timeLoop(AnimatedBgInternal, { refreshRate: fps });
+const checkWebGL = () => {
+    try {
+        const canvas = document.createElement("canvas");
+        return !!(
+            window.WebGLRenderingContext &&
+            (canvas.getContext("webgl") || canvas.getContext("experimental-webgl"))
+        );
+    } catch (e) {
+        return false;
+    }
+}
+
+export const AnimatedBg = checkWebGL() ? timeLoop(AnimatedBgInternal, { refreshRate: fps }) : () => <div />;
