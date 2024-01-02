77b76deea2ddea7779a229142ac3eb6f85ee7c81
Stanisław Zagórowski <hasohasacz@gmail.com>
2024-01-02 01:26:23 +0100

    nix: update stable channel

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "All the components of duckonaut.dev";
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-23.05";
+    nixpkgs.url = "nixpkgs/nixos-23.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
