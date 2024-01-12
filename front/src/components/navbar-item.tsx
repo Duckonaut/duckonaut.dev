6b068ccd9156de8515203d0000c3a2a8ecb9c5fb
Stanisław Zagórowski <hasohasacz@gmail.com>
2024-01-12 23:24:47 +0100

    luax: just reluax

--- front/src/components/navbar-item.tsx
+++
@@ -1,8 +0,0 @@
-import * as preact from "preact";
-
-export function NavbarItem(props: any) {
-  const { name, href, children, rel } = props;
-
-  return (preact.createElement("div", { className: "navbar-item" },
-    preact.createElement("a", { className: "navbar-item-" + name, href: href, rel: rel }, children)));
-}
