ddf436a9e4deb65dfb0375b52d590a7a253b2084
Stanisław Zagórowski <hasohasacz@gmail.com>
2024-01-14 17:52:32 +0100

    blog: fix blog page

--- luax/blog/posts.lua
+++ luax/blog/posts.lua
@@ -3,6 +3,6 @@ return {
         title = "I (re)made JSX for Lua",
         slug = 'building-reluax',
         date = os.time({year=2024, month=1, day=13}),
-        description = "And rebuilt my website with it.",
+        description = "Because it was too good to pass up.",
     },
 }
